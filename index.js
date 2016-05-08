68821cc5271cdf1e046857ec443f45109005a446
Robert Ta <taduydeit@gmail.com>
2016-05-08 17:14:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'S64Q3RXYM7.cirpack.tma.vn.pushNotificationTest',
+  masterKey: process.env.MASTER_KEY || 'master', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://pacific-brushlands-55337.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
