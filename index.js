5d25dc2d560881ebc0d5ffff2f9f30240b807f35
Robert Ta <taduydeit@gmail.com>
2016-07-28 07:50:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
+var uq_qa1 = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'cirpack.tma.vn.pushNotificationTest',
+  verbose: true,
   push: {
     android: {
         senderId: '1042768746539',
@@ -32,18 +33,48 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
+
+var uq_smart = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'smartuq-parse',
+  verbose: true,
+  push: {
+    android: {
+        senderId: '422866127995',
+        apiKey: 'AIzaSyDUddRXbj0pR83WGXfJAv2U7t_ZjytfJ0A'
+    },
+    ios:{
+      pfx:"public/PushNotification_smart.p12",
+      bundleId: "tma.cirpack.smartuq",
+      production : false
+    }
+  },
+  masterKey: process.env.MASTER_KEY || 'master', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://pacific-brushlands-55337.herokuapp.com/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
+//app.use('/uq_smart', uq_smart);
+//app.use('/parse', uq_qa1);
+
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
-// Serve the Parse API on the /parse URL prefix
+// // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+
+app.use("/parse", uq_qa1);
+app.use("/smart", uq_smart);
+
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
@@ -56,7 +87,7 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 80;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
